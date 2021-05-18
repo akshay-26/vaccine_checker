b3f0b417c8e179b0e0fab824e9632027d4a13d14
akshay-26 <akshaykulkarni.bk20@gmail.com>
2021-05-18 18:37:02 +0530

    modified python file

--- vaccine_checker.py
+++ vaccine_checker.py
@@ -5,10 +5,10 @@ import requests
 import re
 from twilio.rest import Client
 
-account_sid = '<twilio sid here>'
-auth_token = '<auth token here>'
-account_phone_number = '<twilio number here>'
-phone_number_to_call = '<your number here>'
+account_sid = 'ACe8bfebd12cb777d971390c9b61d55d8c'
+auth_token = 'ea9dd15ccd128391f95ff165d42c2c78'
+account_phone_number = '+14153197684'
+phone_number_to_call = '+919480798822'
 
 # T0 get the area code:
 # 1) Get the state code under https://cdn-api.co-vin.in/api/v2/admin/location/states
@@ -48,7 +48,7 @@ def check_availability(code, vaccine_center_list):
     for center in vaccine_center_list['centers']:
         if 'sessions' in center:
             for session in center['sessions']:
-                if(session['available_capacity'] > 0 and session['min_age_limit'] < 45):
+                if(session['available_capacity'] > 0 and session['min_age_limit'] >= 45):
                     print('{} Yeppi vaccine available :) for {}. Search code: {}, {}, {}, {}, {}'.format(
                         datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
                         session['date'],
@@ -58,20 +58,20 @@ def check_availability(code, vaccine_center_list):
                         center['name'],
                         center['address']))
                     if(session['available_capacity'] > 10):
-                        callme('We have found {} {} vaccine available at {}, {}'.format(
+                        callme('We have found {} {} vaccine available at {}, {}, {}'.format(
                             session['available_capacity'],
                             session['vaccine'],
                             center['name'],
-                            center['address']))
+                            center['address'],
+                            center['pincode']))
 
 
 def callme(message):
     if bool(re.match("^[A-Za-z0-9]*$", account_sid)) and \
             bool(re.match("^[A-Za-z0-9]*$", account_sid)):
         client = Client(account_sid, auth_token)
-        client.calls.create(
-            twiml='''<Response><Say>Hi there!! {}. \
-               Get vaccinated, stay healthy  </Say></Response>'''.format(message),
+        client.messages.create(
+            body=message,
             to=phone_number_to_call,
             from_=account_phone_number
         )
